2f96aad43adc4dd77c0bbe48868b82a031d4007c
Paulo César Moraes <105931834+pcmoraesmenezes@users.noreply.github.com>
2022-11-22 00:55:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Dicionarios em python funcionam de maneiras semelhante a arrays em javascript, voce possui uma chave e essa chave tem seu valor logo em sequencia, sua estrutura de formação e dada da seguinte forma: d = {}, voce tera um dicionario, porem sem nenhuma chave e sem nenhum valor
+v = {}
+v ["Paulo"] = 18
+print(v)
+#se voce printar a chave "Paulo" a sua saida sera o seu valor
+print(v["Paulo"])
+#É possivel criar novas chaves e valores em nosso dicionario
+v["Felipe"] = 18
+print(v)
+print(v["Felipe"])
